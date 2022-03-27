233e4678ffbd18c4dc9d1ca72bfed443e428fa2f
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-03-27 17:57:00 +0300

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,29 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double diffAndDiv(double first, double second) {
+        return diff(first, second)
+                + div(first, second);
+    }
+
+    public static double allOperations(double first, double second) {
+        return sum(first, second)
+                + diff(first, second)
+                + multiply(first, second)
+                + div(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + diffAndDiv(20, 10));
+        System.out.println("Результат расчета равен: " + allOperations(20, 10));
+    }
+}
