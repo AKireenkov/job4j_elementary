2393b02273357422ea24a0f25dd142df7a50272d
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-26 21:52:49 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,5 +1,8 @@
 package ru.job4j.array;
 
+import java.io.FileOutputStream;
+import java.util.Arrays;
+
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
@@ -7,6 +10,12 @@ public class Matrix {
             for (int j = 0; j < size; j++) {
                 array[i][j] = (i + 1) * (j + 1);
             }
+        }
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(Arrays.deepToString(array).getBytes());
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return array;
     }
